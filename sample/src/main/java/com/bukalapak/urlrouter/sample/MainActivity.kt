e7881a0ba960ea0b403d22e2493c6ef49fe10c97
arifinbardansyah <arifin.bardansyah@bukalapak.com>
2018-07-27 12:25:28 +0700

    sample subdomain support

--- sample/src/main/java/com/bukalapak/urlrouter/sample/MainActivity.kt
+++ sample/src/main/java/com/bukalapak/urlrouter/sample/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 import com.bukalapak.urlrouter.Router
 import com.bukalapak.urlrouter.RouterMap
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,92 +36,100 @@ class MainActivity : AppCompatActivity() {
 
         val firstMap = mutableListOf<RouterMap>()
 
-        // https://first.mysite.com/about
-        firstMap.add(RouterMap(listOf("/about")){
-            displayResult("Open first about page")
+        // https://www.mysite.com/about
+        firstMap.add(RouterMap(listOf("/about")) {
+            displayResult("Open about page")
         })
 
-        // https://first.mysite.com/promo/tas-keren-pria
-        firstMap.add(RouterMap(listOf("/promo/*")){
-            displayResult("Open first promo page")
+        // https://www.mysite.com/promo/tas-keren-pria
+        firstMap.add(RouterMap(listOf("/promo/*")) {
+            displayResult("Open promo page")
         })
 
-        // https://first.mysite.com/promo/tas-keren-pria/discounted
-        firstMap.add(RouterMap(listOf("/promo/*/discounted")){
-            displayResult("Open first discounted promo page")
+        // https://www.mysite.com/promo/tas-keren-pria/discounted
+        firstMap.add(RouterMap(listOf("/promo/*/discounted")) {
+            displayResult("Open discounted promo page")
         })
 
-        // https://first.mysite.com/register?referrer=anonymous
-        firstMap.add(RouterMap(listOf("/register")){
+        // https://www.mysite.com/register?referrer=anonymous
+        firstMap.add(RouterMap(listOf("/register")) {
             val referrer = it.queries.getString("referrer")
-            displayResult("Open first registration page with referrer $referrer")
+            displayResult("Open registration page with referrer $referrer")
         })
 
-        // https://first.mysite.com/transaction/981239/view
-        firstMap.add(RouterMap(listOf("/transaction/<transaction_id>/view")){
+        // https://www.mysite.com/transaction/981239/view
+        firstMap.add(RouterMap(listOf("/transaction/<transaction_id>/view")) {
             val transactionId = it.variables.getLong("transaction_id")
-            displayResult("Open first transaction detail page $transactionId")
+            displayResult("Open transaction detail page $transactionId")
         })
 
-        // https://first.mysite.com/product/kj9fd8-tas-paling-keren-masa-kini
-        firstMap.add(RouterMap(listOf("/product/<product_id:[a-z0-9]+>-*")){
+        // https://www.mysite.com/product/kj9fd8-tas-paling-keren-masa-kini
+        firstMap.add(RouterMap(listOf("/product/<product_id:[a-z0-9]+>-*")) {
             val productId = it.variables.getString("product_id")
-            displayResult("Open first product detail page $productId")
+            displayResult("Open product detail page $productId")
         })
 
-        router.preMap("*://first.mysite.com/*", firstMap) {
+        router.preMap(listOf("*://", ""),
+                listOf("<subdomain:\\[a-z]+>.mysite.com", "mysite.com"),
+                listOf("/*", "", ":<port:[0-9]+>/*", ":<port:[0-9]+>"),
+                firstMap) {
             val subdomain = it.variables.getString("subdomain")
-            if (subdomain == "blog") {
+            if (subdomain == "www") {
+                var url = it.url
+                if (!Pattern.matches("\\w+://.+", it.url)) {
+                    url = "https://" + url
+                }
+                Uri.parse(url).path // Continue routing
+            } else {
                 displayResult("Launch intent: " + it.url)
                 null // Don't continue routing
-            } else {
-                Uri.parse(it.url).path // Continue routing
             }
         }
 
         val secondMap = mutableListOf<RouterMap>()
 
         // https://second.mysite.com/about
-        secondMap.add(RouterMap(listOf("/about")){
+        secondMap.add(RouterMap(listOf("/about")) {
             displayResult("Open second about page")
         })
 
         // https://second.mysite.com/promo/tas-keren-pria
-        secondMap.add(RouterMap(listOf("/promo/*")){
+        secondMap.add(RouterMap(listOf("/promo/*")) {
             displayResult("Open second promo page")
         })
 
         // https://second.mysite.com/promo/tas-keren-pria/discounted
-        secondMap.add(RouterMap(listOf("/promo/*/discounted")){
+        secondMap.add(RouterMap(listOf("/promo/*/discounted")) {
             displayResult("Open second discounted promo page")
         })
 
         // https://second.mysite.com/register?referrer=anonymous
-        secondMap.add(RouterMap(listOf("/register")){
+        secondMap.add(RouterMap(listOf("/register")) {
             val referrer = it.queries.getString("referrer")
             displayResult("Open second registration page with referrer $referrer")
         })
 
         // https://second.mysite.com/transaction/981239/view
-        secondMap.add(RouterMap(listOf("/transaction/<transaction_id>/view")){
+        secondMap.add(RouterMap(listOf("/transaction/<transaction_id>/view")) {
             val transactionId = it.variables.getLong("transaction_id")
             displayResult("Open second transaction detail page $transactionId")
         })
 
         // https://second.mysite.com/product/kj9fd8-tas-paling-keren-masa-kini
-        secondMap.add(RouterMap(listOf("/product/<product_id:[a-z0-9]+>-*")){
+        secondMap.add(RouterMap(listOf("/product/<product_id:[a-z0-9]+>-*")) {
             val productId = it.variables.getString("product_id")
             displayResult("Open second product detail page $productId")
         })
 
-        router.preMap("*://second.mysite.com/*", secondMap) {
-            val subdomain = it.variables.getString("subdomain")
-            if (subdomain == "blog") {
-                displayResult("Launch intent: " + it.url)
-                null // Don't continue routing
-            } else {
-                Uri.parse(it.url).path // Continue routing
+        router.preMap(listOf("*://", ""),
+                listOf("second.mysite.com"),
+                listOf("/*", "", ":<port:[0-9]+>/*", ":<port:[0-9]+>"),
+                secondMap) {
+            var url = it.url
+            if (!Pattern.matches("\\w+://.+", it.url)) {
+                url = "https://" + url
             }
+            Uri.parse(url).path // Continue routing
         }
     }
 
