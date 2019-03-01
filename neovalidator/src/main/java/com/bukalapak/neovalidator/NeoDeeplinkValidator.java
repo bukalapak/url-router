8ca8cddbd50207ae0887f1be056ac7f6e29ad159
arifinbardansyah <arifinbardansyah@gmail.com>
2019-03-01 13:58:19 +0700

    add validate premap

--- neovalidator/src/main/java/com/bukalapak/neovalidator/NeoDeeplinkValidator.java
+++ neovalidator/src/main/java/com/bukalapak/neovalidator/NeoDeeplinkValidator.java
@@ -7,23 +7,23 @@ import java.nio.file.Paths;
 public class NeoDeeplinkValidator {
     public static void main(String[] args) {
         if (args.length<2) {
-            System.err.println("missing argument\njava -jar NeoDeeplinkValidator.jar configId filePath");
+            System.err.println("missing argument\njava -jar NeoDeeplinkValidator.jar filePath configId");
             System.exit(1);
         }
-        final String configId = args[0];
-        String filePath = args[1];
+        String filePath = args[0];
+        final String configId = args[1];
         try {
             String json = new String(Files.readAllBytes(Paths.get(filePath)));
             DeeplinkValidator.validateDeeplinkConfig(configId, json, new OnDeeplinkCheckListener() {
                 @Override
                 public void onDeeplinkValid() {
-                    System.out.println("VALID JSON, key: " + configId);
+                    System.out.println("VALID JSON, configid: " + configId);
                     System.exit(0);
                 }
 
                 @Override
                 public void onDeeplinkInvalid(Exception e) {
-                    System.err.println("INVALID JSON, key: " + configId);
+                    System.err.println("INVALID JSON, configid: " + configId);
                     e.printStackTrace();
                     System.exit(1);
                 }
