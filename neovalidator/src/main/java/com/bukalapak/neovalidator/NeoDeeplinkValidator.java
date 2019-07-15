7bd322bca122b7a8dac7be31fc3252bc60a0c4d2
arifinbardansyah <arifinbardansyah@gmail.com>
2019-07-15 17:52:21 +0700

    update validator param name

--- neovalidator/src/main/java/com/bukalapak/neovalidator/NeoDeeplinkValidator.java
+++ neovalidator/src/main/java/com/bukalapak/neovalidator/NeoDeeplinkValidator.java
@@ -7,23 +7,23 @@ import java.nio.file.Paths;
 public class NeoDeeplinkValidator {
     public static void main(String[] args) {
         if (args.length<2) {
-            System.err.println("missing argument\njava -jar NeoDeeplinkValidator.jar filePath configId");
+            System.err.println("missing argument\njava -jar NeoDeeplinkValidator.jar filePath validatorName");
             System.exit(1);
         }
         String filePath = args[0];
-        final String configId = args[1];
+        final String validatorName = args[1];
         try {
             String json = new String(Files.readAllBytes(Paths.get(filePath)));
-            DeeplinkValidator.validateDeeplinkConfig(configId, json, new OnDeeplinkCheckListener() {
+            DeeplinkValidator.validateDeeplinkConfig(validatorName, json, new OnDeeplinkCheckListener() {
                 @Override
                 public void onDeeplinkValid() {
-                    System.out.println("VALID JSON, config id: " + configId);
+                    System.out.println("VALID JSON, validator name: " + validatorName);
                     System.exit(0);
                 }
 
                 @Override
                 public void onDeeplinkInvalid(Exception e) {
-                    System.err.println("INVALID JSON, config id: " + configId);
+                    System.err.println("INVALID JSON, validator name: " + validatorName);
                     System.err.println(e.getMessage());
                     System.exit(1);
                 }
